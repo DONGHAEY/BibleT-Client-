5ec34b17441d2823a9f7d5bcefd281dae2470baa
DONGHAEY <dongheaey@gmail.com>
2022-12-31 00:28:05 +0900

    members page 여러번 실행되는 부분을 고침 또한 관리자는 멤버들의 역할을 업데이트 할 수 있도록 인터페이스를 구성

--- src/page/TrainMembers.jsx
+++ src/page/TrainMembers.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
@@ -11,63 +11,75 @@ import { AddCircleButton } from "../styledComponent/AddCircleButton";
 import { HiOutlineUserAdd } from "@react-icons/all-files/hi/HiOutlineUserAdd";
 import { useRecoilState } from "recoil";
 import { TrainMembersState } from "../store/TrainMembersStore";
+import { TrainProfileState } from "../store/TrainProfileState";
+import { useMemo } from "react";
 
-const Members = ({ train, navigate, trainProfile }) => {
+const Members = ({ train, navigate }) => {
   const { trainId } = useParams();
   const [members, setTrainMembers] = useRecoilState(TrainMembersState);
+  const [trainProfile, setTrainProfile] = useRecoilState(TrainProfileState);
 
-  useEffect(() => {
-    (async () => {
-      const trainMembers = await axios.get(
-        `/api/train/${trainId}/trainMemberProfiles`
-      );
-      setTrainMembers(trainMembers.data);
-    })();
-  }, []);
-
-  const MemberProfiles = members.map((member, memIdx) => {
+  const MemberProfiles = useMemo(() => {
     return (
-      <MemberDiv
-        key={memIdx}
-        onClick={() => {
-          navigate(`/train/${member.trainId}/${member.userId}`);
-        }}
-      >
-        <img
-          src={member.profileImage}
-          style={{ width: "50px", height: "50px", borderRadius: "100%" }}
-        ></img>
-        <span style={{ fontSize: "15px", width: "100px" }}>
-          <span style={{ fontWeight: "bold" }}>{member.nickName}</span>
-          <br></br>
-          <span>{role[member.role]}</span>
-        </span>
-        <div>{member.checkStamps.length}번</div>
-      </MemberDiv>
-    );
-  });
-
-  return (
-    trainProfile && (
-      <FlexWrapper>
-        {MemberProfiles}
-        {trainProfile?.role === "ROLE_CAPTAIN" ? (
-          <AddCircleButton
+      members.length &&
+      members.map((member, memIdx) => {
+        const roleKeys = Object.keys(role);
+        console.log(member);
+        return (
+          <MemberDiv
+            key={memIdx}
             onClick={() => {
-              const tempInput = document.createElement("input");
-              tempInput.value = `http://${window.location.host}/joinTrain/${trainId}?joinKey=${train.joinKey}`;
-              document.body.appendChild(tempInput);
-              tempInput.select();
-              document.execCommand("copy");
-              document.body.removeChild(tempInput);
-              alert("초대링크가 복사되었습니다");
+              navigate(`/train/${member?.trainId}/${member?.userId}`);
             }}
           >
-            <HiOutlineUserAdd size={25} />
-          </AddCircleButton>
-        ) : null}
-      </FlexWrapper>
-    )
+            <img
+              src={member?.profileImage}
+              style={{ width: "50px", height: "50px", borderRadius: "100%" }}
+            ></img>
+            <span style={{ fontSize: "15px", width: "100px" }}>
+              <span style={{ fontWeight: "bold" }}>{member?.nickName}</span>
+              <br></br>
+              {trainProfile?.role !== "ROLE_CAPTAIN" ||
+              member?.userId === trainProfile.userId ? (
+                <span>{role[member?.role]}</span>
+              ) : (
+                <select defaultValue={member?.role}>
+                  {roleKeys.map((roleKey, idx) => {
+                    return (
+                      <option key={idx} value={roleKey}>
+                        {role[roleKey]}
+                      </option>
+                    );
+                  })}
+                </select>
+              )}
+            </span>
+            <div>{member?.checkStamps.length}번</div>
+          </MemberDiv>
+        );
+      })
+    );
+  }, [members]);
+
+  return (
+    <FlexWrapper>
+      {MemberProfiles}
+      {trainProfile?.role === "ROLE_CAPTAIN" ? (
+        <AddCircleButton
+          onClick={() => {
+            const tempInput = document.createElement("input");
+            tempInput.value = `http://${window.location.host}/joinTrain/${trainId}?joinKey=${train.joinKey}`;
+            document.body.appendChild(tempInput);
+            tempInput.select();
+            document.execCommand("copy");
+            document.body.removeChild(tempInput);
+            alert("초대링크가 복사되었습니다");
+          }}
+        >
+          <HiOutlineUserAdd size={25} />
+        </AddCircleButton>
+      ) : null}
+    </FlexWrapper>
   );
 };
 
